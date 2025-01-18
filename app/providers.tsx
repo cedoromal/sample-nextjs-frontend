415269b521335440e70e2eb359da7f89a6ff0d3e
Carl Eric Doromal <dev@cedoromal.com>
2025-01-18 11:46:55 +0000

    Add ApiContext Provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -8,6 +8,8 @@ import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { Configuration } from "@/openapi";
+
 export interface ProvidersProps {
   children: React.ReactNode;
   themeProps?: ThemeProviderProps;
@@ -23,11 +25,32 @@ declare module "@react-types/shared" {
 
 const queryClient = new QueryClient();
 
+const defaultClientApiConfig = new Configuration({
+  basePath: typeof window !== "undefined" ? window.location.origin : undefined,
+});
+
+export const ApiContext = React.createContext({
+  apiConfig: defaultClientApiConfig,
+  setApiConfig: (_apiConfig: Configuration) => {},
+});
+
 export function Providers({ children, themeProps }: ProvidersProps) {
   const router = useRouter();
 
+  const [apiConfig, setApiConfig] = React.useState(defaultClientApiConfig);
+
   return (
     <QueryClientProvider client={queryClient}>
+      <ApiContext.Provider
+        value={{
+          apiConfig: apiConfig,
+          setApiConfig: (apiConfig: Configuration) => setApiConfig(apiConfig),
+        }}
+      >
+        <HeroUIProvider navigate={router.push}>
+          <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+        </HeroUIProvider>
+      </ApiContext.Provider>
     </QueryClientProvider>
   );
 }
